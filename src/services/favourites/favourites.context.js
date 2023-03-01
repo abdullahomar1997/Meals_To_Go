3dfcbf788b06c339887626ffc2f83cc07bb34e05
abdullahomar1997 <abdullahomar19971201@gmail.com>
2023-03-01 04:32:43 +0200

    storing favourites

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { createContext, useEffect, useState } from 'react'
 
 export const FavouritesContext = createContext();
 
@@ -12,6 +13,35 @@ export const FavouritesContextProvider = ({ children }) => {
     const remove = (restaurant) => {
         setFavourites(favourites.filter((x) => x.placeId != restaurant.placeId))
     }
+
+    const saveFavourites = async (value) => {
+        try {
+            const jsonValue = JSON.stringify(value)
+            await AsyncStorage.setItem('@favourites', jsonValue)
+        } catch (e) {
+            console.log("error storing", e)
+        }
+    }
+
+
+    const loadFavourites = async () => {
+        try {
+            const jsonValue = await AsyncStorage.getItem('@favourites')
+            return jsonValue != null ? setFavourites(JSON.parse(jsonValue)) : null;
+        } catch (e) {
+            // error reading value]
+            console.log("error loading", e)
+
+        }
+    }
+
+    useEffect(() => {
+        saveFavourites(favourites)
+    }, [favourites])
+
+    useEffect(() => {
+        loadFavourites();
+    }, [])
 
     return (
         <FavouritesContext.Provider
