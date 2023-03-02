20d81da313252e2ef3d1ee7034b9d7594c795c7b
abdullahomar1997 <abdullahomar19971201@gmail.com>
2023-03-02 02:28:52 +0200

    configured settings feature

--- src/infastructure/navigation/app.navigator.js
+++ src/infastructure/navigation/app.navigator.js
@@ -2,7 +2,6 @@ import React, { useContext } from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-import { Button, Text } from 'react-native';
 import { SafeArea } from '../../components/utils/safe-area.components2';
 import { RestaurantsNavigator } from './restaurants.navigator';
 import { MapScreen } from '../../features/map/screens/map.screen';
@@ -10,6 +9,8 @@ import { AuthenticationContext } from '../../services/authentication/authenticat
 import { FavouritesContextProvider } from '../../services/favourites/favourites.context';
 import { LocationContextProvider } from '../../services/location/location.context';
 import { RestaurantsContextProvider } from '../../services/restaurant/restaurants.context';
+import { SettingsScreen } from '../../features/settings/screens/settings.screen';
+import { SettingsNavigator } from './settings.navigator';
 
 const Tab = createBottomTabNavigator();
 
@@ -32,16 +33,6 @@ const createScreenOptions = ({ route }) => {
     }
 }
 
-const Settings = () => {
-    const { onLogout } = useContext(AuthenticationContext)
-    return (
-        <SafeArea>
-            <Text>Settnhhhings</Text>
-            <Button title="logout" onPress={() => onLogout()} />
-        </SafeArea>
-    )
-}
-
 export const AppNavigator = () => (
     <FavouritesContextProvider>
         <LocationContextProvider>
@@ -49,7 +40,7 @@ export const AppNavigator = () => (
                 <Tab.Navigator screenOptions={createScreenOptions}>
                     <Tab.Screen name="Restaurant" component={RestaurantsNavigator} />
                     <Tab.Screen name="Map" component={MapScreen} />
-                    <Tab.Screen name="Settings" component={Settings} />
+                    <Tab.Screen name="Settings" component={SettingsNavigator} />
                 </Tab.Navigator>
             </RestaurantsContextProvider>
         </LocationContextProvider>
---
+++ src/infastructure/navigation/settings.navigator.js
@@ -0,0 +1,35 @@
+import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack'
+import React, { useEffect } from 'react'
+import { SettingsScreen } from '../../features/settings/screens/settings.screen';
+
+const SettingsStack = createStackNavigator();
+
+export const SettingsNavigator = ({ route, navigation }) => {
+    return (
+        <SettingsStack.Navigator
+            screenOptions={{
+                cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+            }}
+        >
+            <SettingsStack.Screen
+
+                options={{
+                    header: () => null,
+                }}
+                name="Settings"
+                component={SettingsScreen}
+            >
+
+            </SettingsStack.Screen>
+
+            <SettingsStack.Screen
+                name="Favourites"
+                component={FavouritesScreen}
+            >
+
+            </SettingsStack.Screen>
+
+        </SettingsStack.Navigator>
+    )
+}
+
